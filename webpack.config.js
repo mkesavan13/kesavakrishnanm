a111733d2aa9d46fafc5e7560256f9965111aaad
Kesava Krishnan Madavan <kesavakrishnan.m@zohocorp.com>
2020-05-25 18:08:49 +0530

    Webpack config changed 1. Param to know if devserver or prod build 2. Copy pattern added for image along with js 3. WriteFile added for image 4. development or not sent in LessThemingPlugin constructor

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,79 @@
+/*
+    ****************************************
+    **    Project setup by,               **
+    **    Kesava Krishnan Madavan         **
+    **    https://github.com/mkesavan13   **
+    ****************************************
+                                  *
+                                *
+            *                 *      * 
+          *                 *          *
+        *                 *              *
+      *                 *                  *
+        *             *                  *
+          *         *                  *
+            *     *                  *
+                *
+              *
+            *
+*/
+
+
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const WriteFilePlugin = require('write-file-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const {LessThemingPlugin} = require('./build/LessThemingPlugin.js');
+const { LessThemingPlugin } = require('./build/LessThemingPlugin.js');
 
-const paths={
-  src: path.join(__dirname, 'src'),
-  dist: path.join(__dirname, 'dist'),
+const my_web_config={
+  paths:{
+    src: path.join(__dirname, 'src'),
+    dist: path.join(__dirname, 'dist'),
+  },
+  development: process.env.npm_lifecycle_event === 'start' ? true : false //Determines if it is development build or production build
 }
 
 module.exports = {
   devServer:{
-    contentBase: paths.dist,
+    contentBase: my_web_config.paths.dist,
     compress: true,
     port: 9000
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.join(paths.src,'index.html'),
+      template: path.join(my_web_config.paths.src,'index.html'),
       filename: 'index.html',
     }),
-    new CopyPlugin({
+    new CopyPlugin({ //Production
       patterns: [
-        { from: path.join(paths.src,'assets'), to: 'assets' },
-      ],
+        {
+          from: path.join(my_web_config.paths.src,'assets','js'),
+          to: path.join('assets','js')
+        },
+        {
+          from: path.join(my_web_config.paths.src,'assets','images'),
+          to: path.join('assets','images')
+        }
+      ]
     }),
-    new WriteFilePlugin({
+    new WriteFilePlugin({ //Development
         test: new RegExp(path.join('assets','js')),
         useHashIndex: true
     }),
+    new WriteFilePlugin({ //Development
+        test: new RegExp(path.join('assets','images')),
+        useHashIndex: true
+    }),
     new LessThemingPlugin({
-      src: path.join(paths.src,'assets/css/themes'),
-      dist: path.join(paths.dist,'assets/css/compiledThemes'),
+      src: path.join(my_web_config.paths.src,'assets/css/themes'),
+      dist: path.join(my_web_config.paths.dist,'assets/css/compiledThemes'),
       exclude:[
         'dark'
-      ]
+      ],
+      development: my_web_config.development
     })
   ]
 };
